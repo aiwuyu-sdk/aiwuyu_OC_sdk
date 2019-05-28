753547703ecc884e3b59536f961bd55258b5ed32
yhl <yuhongli@aiwuyu.com>
2019-05-28 11:27:49 +0800

    1.0.7

---
+++ aiwuyu_OC_sdkDemo/aiwuyu_OC_sdk/aiwuyu_OC_sdk.framework/Headers/aiwuyu_OC_sdk.h
@@ -0,0 +1,22 @@
+//
+//  aiwuyu_OC_sdk.h
+//  aiwuyu_OC_sdk
+//
+//  Created by 于洪礼 on 2019/5/21.
+//  Copyright © 2019 awy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <aiwuyu_OC_sdk/AwySDK.h>
+
+
+//! Project version number for aiwuyu_OC_sdk.
+FOUNDATION_EXPORT double aiwuyu_OC_sdkVersionNumber;
+
+//! Project version string for aiwuyu_OC_sdk.
+FOUNDATION_EXPORT const unsigned char aiwuyu_OC_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <aiwuyu_OC_sdk/PublicHeader.h>
+
+
